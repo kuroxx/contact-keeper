3d8440fcbc13acd307c875e7d56d299d2062b91d
Anni <angelblue1031@gmail.com>
2020-02-18 03:14:51 +0800

    Added contactForm component

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -38,9 +38,13 @@ const ContactState = props => {
     ]
   };
 
-  const [state, dispath] = useReducer(contactReducer, initialState);
+  const [state, dispatch] = useReducer(contactReducer, initialState);
 
   // Add Contact
+  const addContact = contact => {
+    contact.id = uuid.v4();
+    dispatch({type: ADD_CONTACT, payload: contact});
+  }
 
   // Delete Contact
 
@@ -56,7 +60,10 @@ const ContactState = props => {
 
   return ( 
     <ContactContext.Provider 
-      value={{contacts: state.contacts}} 
+      value={{
+        contacts: state.contacts,
+        addContact
+      }} 
     >
     {props.children} 
     </ContactContext.Provider>
